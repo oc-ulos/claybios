c187d7c025653315dea652ae60457f933595c728
Ocawesome101 <ocawesome101@gmail.com>
2023-08-09 16:40:23 -0400

    add boot device selection persistence

--- src/menu.lua
+++ src/menu.lua
@@ -1,15 +1,19 @@
 -- menus?
 
+local eeprom=component.proxy((component.list("eeprom")()))
 local bootable={}
+local selected=0
 for i=1,#drives do
   local drive=drives[i]
   for j=1,#drive.ptt do
     if drive.ptt[j].boot then
       bootable[#bootable+1]={drive=drive.drive,start=drive.ptt[j].start,size=drive.ptt[j].size,type=drive.ptt[j].type}
+      if drive.address == eeprom.getData() then
+        selected = #bootable
+      end
     end
   end
 end
-local selected=1
 write("")
 if #bootable==0 then
   computer.beep("....")
@@ -21,17 +25,21 @@ elseif #bootable>1 then
     local b=bootable[i]
     write(("%d) %s (%s)"):format(i,b.drive.address:sub(1,8),b.type))
   end
+  if selected>0 then
+    write("Will default to "..selected.." if no choice is made")
+  end
   repeat
-    local signal=table.pack(computer.pullSignal())
+    local signal=table.pack(computer.pullSignal(5))
     local char
     if signal[1]=="key_down" then
       char=string.char(signal[3])
+      selected=tonumber(char)
     end
-    selected=tonumber(char or "0")
-  until char and bootable[selected]
+  until char or bootable[selected]
 end
 local boot = bootable[selected]
 write("Booting from "..boot.drive.address:sub(1,8))
+eeprom.setData(boot.drive.address)
 function computer.getBootAddress()return boot.drive.address end
 local ok,err=xpcall(function()
   assert(load((readers[boot.type] or readers.generic)(boot),"="..boot.drive.address:sub(1,8)))()
