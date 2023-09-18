41e62ddffff4dcf5f2acd1877bea251ca697aeed
Ocawesome101 <ocawesome101@gmail.com>
2023-09-18 00:15:24 -0400

    oops, fix build

--- tools/build.sh
+++ tools/build.sh
@@ -4,8 +4,8 @@ tools/preproc.lua src/main.lua /tmp/built.lua -strip-comments
 rm -rf out
 mkdir -p out/usr/share/prismbios
 tools/zlua.lua PrismBIOS < /tmp/built.lua > out/usr/share/prismbios/bios.lua
-if [ $(stat -c %s bios.lua) -gt 4096 ]; then
+if [ $(stat -c %s out/usr/share/prismbios/bios.lua) -gt 4096 ]; then
   echo WARNING: bios.lua larger than 4096 bytes
 else
-  echo bios.lua is $(stat -c %s bios.lua) bytes
+  echo bios.lua is $(stat -c %s out/usr/share/prismbios/bios.lua) bytes
 fi
